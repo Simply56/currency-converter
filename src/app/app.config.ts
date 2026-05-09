d84d4f7d0f72ecbd217396246601b91d21ab0bec
Martin Srsen <xsrsen1@fi.muni.cz>
2026-05-09 20:56:04 +0200

    go zoneless

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,11 @@
-import { ApplicationConfig, isDevMode, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, isDevMode, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideServiceWorker } from '@angular/service-worker';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideZonelessChangeDetection(),
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideServiceWorker('ngsw-worker.js', {
